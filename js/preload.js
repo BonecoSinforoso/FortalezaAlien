a7239ab694f78f58a819c50c7a042e7e9836e151
Boneco Sinforoso <icmgc98@gmail.com>
2023-06-14 21:14:08 -0300

    exclusao quase completa

--- js/preload.js
+++ js/preload.js
@@ -13,21 +13,25 @@ class preload extends Phaser.Scene
 
     preload()
     {
-        this.load.image("chao2","/assets/LabTileset/Lab/chao2.png")
+        this.load.image("porta","/assets/LabTileset/Lab/555-0.png")
+        this.load.image("janela","/assets/LabTileset/Lab/572-5.png")
+        this.load.image("bateria","/assets/LabTileset/Lab/859-1.png")
         this.load.image("mesa1","/assets/LabTileset/Lab/862-0.png")
-        this.load.image("prateleira","/assets/LabTileset/Lab/1005-0.png")
+        this.load.image("barreira","/assets/LabTileset/Lab/931-0.png")
         this.load.image("plataforma2","/assets/LabTileset/Lab/952-0.png")
         this.load.image("ativador","/assets/LabTileset/Lab/961-0.png")
+        this.load.image("prateleira","/assets/LabTileset/Lab/1005-0.png")
+
+        this.load.image("fundo2", "/assets/LabTileset/Backgrounds/984-0.png")
+
+        this.load.image("chao2","/assets/LabTileset/Lab/chao2.png")        
         this.load.image("laser","/assets/LabTileset/Lab/laser3.png")
         this.load.image("ChaveTeste","/assets/LabTileset/Lab/chaveBoa.png")
-        this.load.image("porta","/assets/LabTileset/Lab/555-0.png")
         this.load.image("cientista","/assets/LabTileset/Lab/cientista.png")
-        this.load.image("bateria","/assets/LabTileset/Lab/859-1.png")
-        this.load.image("janela","/assets/LabTileset/Lab/572-5.png")
-        this.load.image("barreira","/assets/LabTileset/Lab/931-0.png")
+        
         this.load.image("fundo", "/assets/_fundo.png")
         this.load.image("fundoMenu", "/assets/_fundoMenu.jpg")
-        this.load.image("fundo2", "/assets/LabTileset/Backgrounds/984-0.png")
+        
         this.load.image("btn_play", "/assets/_btn_play02.png")
         this.load.spritesheet("player", "assets/_alien.png", { frameWidth: 25, frameHeight: 35})
         this.load.image("inimigo","/assets/inimigo.png")
